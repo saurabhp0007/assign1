702db7569d7beda62377ffa4c53805615f55b44e
saurabh kumar pandey <saurabhpanday21212@gmail.com>
2024-04-10 16:18:43 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Login from './Components/Login'
-import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import LoginPage from './Page/LoginPage'
 import Register from './Page/Register'
 import Profile from './Page/Profile'
@@ -28,9 +28,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-      <RouterProvider routes={routes} />
-    </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   )
 }
